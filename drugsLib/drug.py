9b7bed6275f5e6d5d62baaee35a30cc3b6fdfab6
Michael Douglass <msd90@case.edu>
2018-09-10 17:40:42 -0400

    Cleans syntax and descriptions for classes and functions

--- drugsLib/drug.py
+++ drugsLib/drug.py
@@ -1,29 +1,51 @@
-# drug class for initializing drugs
-
 import numpy as np
 
 def statesFromBits(bit, N):
+    """Creates binary strings from number input
+
+    Args:
+        bit (int): length of array
+        N (int): Zero padding returned
+    Returns:
+        states (list): list of binary strings from 0 to bit-1
+    """
     states = []
-    for i in range(16):
+    for i in range(bit):
         b = format(i, 'b')
         bB = b.zfill(N)
         states.append(bB)
     return states
 
-# Define the Hamming Distance
+
 def hamDist(Si, Sj):
+    """ Return the Hamming distance between two bits
+
+    Args:
+        Si (string): binary value
+        Sj (string): binary value
+    Returns:
+        Hamming Distance
+    """
     return sum(ei != ej for ei, ej in zip(Si, Sj))
 
-# Make a drug class
+
 class Drug:
-    # This will make it easy to call certain qualities
-    # Arguments:
-    # name is a string
-    # Fit is an array of the fitness values
-    # States is the binary genotype that corresponds to the bins
-    # R is the biasing variable that we set to 0
+    """ Drug class main takes fitness landscape and returns probability matrix.
+
+    Args:
+        name (string): Full name of the Drug
+        Fit (list or array): Fitness values for drug corresponding to the
+                             states given (binary)
+        States (list): binary value strings must be same length as Fitness
+        LFP (string): binary genotype corresponding to lowest fitness
+                      peak.
+        R (float): biasing variable described in Markov model
+    Attributes:
+        LPFf (float): fitness value of LPF states
+        tMat(matrix): probability matrix from markov model of dimensions
+                      [states x states]
+    """
     def __init__(self, name, Fit, states, LFP, R=0):
-        # @par
         self.name = name
         self.Fit = np.array(Fit)
         self.states = states

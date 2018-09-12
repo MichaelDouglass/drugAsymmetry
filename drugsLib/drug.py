2bc583a6a2e9dda5e57ba51ee35f632e674ffc31
Michael Douglass <msd90@case.edu>
2018-09-11 23:19:17 -0400

    Incorporates new functions to analyze data csvExplore and matrixPlotter

--- drugsLib/drug.py
+++ drugsLib/drug.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 def statesFromBits(bit, N):
     """Creates binary strings from number input
@@ -27,6 +30,155 @@ def hamDist(Si, Sj):
         Hamming Distance
     """
     return sum(ei != ej for ei, ej in zip(Si, Sj))
+
+
+def csvExplore(Drug1, Drug2, Drug3, f0=None, f1=None, finf=None, cols=None):
+    """Used to show all data from given csv file_extension
+
+    Args:
+    Returns:
+    """
+    nameD1 = Drug1.name
+    nameD2 = Drug2.name
+    nameD3 = Drug3.name
+
+    if cols == None:
+        col_names=['Final Drug', 'Steering Drug 1', 'Steering Drug 1 Amount', 'Steering Drug 2', 'Steering Drug 2 Amount',
+                   'Drug1:Drug2', 'Drug1+Drug2', 'Fitness of infinite apps', 'Lowest Fitness Achieved',
+                   'Maximum Simulated Drug Application', 'R-Value', 'epsilon', 'rho', 'theta']
+    else:
+        col_names = cols
+
+    if f0 == None:
+        filename = 'R0-10_AllDrugs_OnlyCountedWithin95perc_app10_eps_NoThirdInf.csv'
+    else:
+        filename = f0
+
+    df0 = pd.read_csv(filename, names=col_names)
+    df0 = df0[df0['Final Drug'] == nameD3]
+    df0 = df0[(df0['Steering Drug 1'] == nameD1) & (df0['Steering Drug 2'] == nameD2)]
+
+    if f1 == None:
+        filename = 'R0-10_AllDrugs_OnlyCountedWithin95perc_app10_eps_ThirdOnceInf.csv'
+    else:
+        filename = f1
+    df1 = pd.read_csv(filename, names=col_names)
+    df1 = df1[df1['Final Drug'] == nameD3]
+    df1 = df1[(df1['Steering Drug 1'] == nameD1) & (df1['Steering Drug 2'] == nameD2)]
+
+    if finf == None:
+        filename = 'R0-10_AllDrugs_OnlyCountedWithin95perc_app10_eps_ThirdInf.csv'
+    else:
+        filename = finf
+    dfInf = pd.read_csv(filename, names=col_names)
+    dfInf = dfInf[dfInf['Final Drug'] == nameD3]
+    dfInf = dfInf[(dfInf['Steering Drug 1'] == nameD1) & (dfInf['Steering Drug 2'] == nameD2)]
+
+    dataFrames = [df0, df1, dfInf]
+    fig = plt.figure(figsize=(12,12))
+    count = 331
+
+    try:
+        yminF = min([min(df0['Lowest Fitness Achieved']), min(df1['Lowest Fitness Achieved']), min(dfInf['Lowest Fitness Achieved'])])
+    except:
+        yminF = 0
+
+    try:
+        ymaxF = max([max(df0['Lowest Fitness Achieved']), max(df1['Lowest Fitness Achieved']), max(dfInf['Lowest Fitness Achieved'])])
+    except:
+        ymaxF = 3
+
+
+    for df in dataFrames:
+        ax = fig.add_subplot(count)
+        ax.plot(df['R-Value'], df['Lowest Fitness Achieved'])
+        if count == 331:
+            ax.set_title('Third Drug 0 Applications')
+            ax.set_ylabel('Lowest Fitness Achieved')
+        if count == 332:
+            ax.set_title('Third Drug 1 Applications')
+        if count == 333:
+            ax.set_title('Third Drug Inf Applications')
+        ax.set_ylim([yminF, ymaxF])
+
+        spot2 = count + 3
+
+        ax = fig.add_subplot(spot2)
+        ax.plot(df['R-Value'], df['rho'])
+        if spot2 == 334:
+            ax.set_ylabel('Rho')
+        ax.set_ylim([0, 10])
+
+        spot3 = count + 6
+
+        ax = fig.add_subplot(spot3)
+        ax.plot(df['R-Value'], df['theta']/(np.pi/180))
+        if spot3 == 337:
+            ax.set_ylabel('Theta (deg)')
+        if spot3 == 338:
+            ax.set_xlabel('R-Value')
+        ax.set_ylim([0, 180])
+
+        count = count + 1
+        plt.suptitle(nameD1+'$\Rightarrow$'+nameD2+'$\Rightarrow$'+nameD3, fontweight='bold', fontsize='x-large')
+    return [df0, df1, dfInf, yminF, ymaxF]
+
+
+def matrixPlotter(DRUG1, DRUG2, DRUG, yminF=0, ymaxF=3.4, mod=None, Ap=10, S0=None, states=None):
+
+    if states == None:
+        states = statesFromBits(16, 4)
+    else:
+        states = states
+
+    if S0 == None:
+        S0 = np.array(np.ones(len(states))/2**4)
+    else:
+        S0 = S0
+
+    cmap = plt.cm.inferno
+    norm = mpl.colors.BoundaryNorm(np.arange(yminF,ymaxF,0.01), cmap.N)
+
+    avgMat = np.zeros([Ap+1, Ap+1])
+    if mod == None:
+        for NA in range(0,Ap+1):
+            for NB in range(0,Ap+1):
+                SN = np.array(S0) * DRUG1.tMat**NA * DRUG2.tMat**NB * DRUG.tMat**100
+                AvgFit = np.dot(np.array(SN[0,:]), DRUG.Fit)
+                avgMat[NA, NB] = AvgFit
+    if mod == -1:
+        for NA in range(0,Ap+1):
+            for NB in range(0,Ap+1):
+                if NA == 0 and NB == 0:
+                    SN = np.array(S0) * DRUG.tMat**100
+                elif NA == 0:
+                    SN = np.array(S0) * DRUG2.tMat**NB * DRUG.tMat**100
+                elif NB == 0:
+                    SN = np.array(S0) * DRUG1.tMat**NA * DRUG.tMat**100
+                # SN = np.array(S0) * DRUG1.tMat**NA * DRUG2.tMat**NB * DRUG.tMat**100
+                AvgFit = np.dot(np.array(SN[0,:]), DRUG.Fit)
+                avgMat[NA, NB] = AvgFit
+    if mod == 1:
+        for NA in range(0,Ap+1):
+            for NB in range(0,Ap+1):
+                SN = np.array(S0) * DRUG1.tMat**NA * DRUG2.tMat**NB * DRUG.tMat**1
+                AvgFit = np.dot(np.array(SN[0,:]), DRUG.Fit)
+                avgMat[NA, NB] = AvgFit
+    if mod == 0:
+        for NA in range(0,Ap+1):
+            for NB in range(0,Ap+1):
+                SN = np.array(S0) * DRUG1.tMat**NA * DRUG2.tMat**NB
+                AvgFit = np.dot(np.array(SN[0,:]), DRUG.Fit)
+                avgMat[NA, NB] = AvgFit
+
+    fig = plt.figure(figsize=(6,6))
+    ax = fig.add_subplot(111)
+    Z = ax.matshow(avgMat, cmap=cmap, norm=norm)
+    ax.set_xlabel(DRUG1.name + ' Applied Second N Times')
+    ax.set_ylabel(DRUG2.name + ' Applied First N Times')
+    ax.set_title('Average Fitness <f> for ' + DRUG.name)
+    plt.colorbar(Z)
+    plt.tight_layout()
 
 
 class Drug:
